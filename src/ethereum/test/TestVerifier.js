1586e904148de64f6f09355281e814aa78ba5c57
Mario Gemoll <mario@appliedblockchain.com>
2019-03-27 20:54:28 +0000

    updated test

--- src/ethereum/test/TestVerifier.js
+++ src/ethereum/test/TestVerifier.js
@@ -12,35 +12,31 @@ var IC = [];
 var A_g, A_h, B_g, B_h, C_g, C_h;
 var H, K;
 
-contract('Verifier', function(accounts) {
-	fs.readFile("../../build/vk_data", function(err, data) {
-		if(err) {
+contract('Verifier', function (accounts) {
+	fs.readFile("../../build/vk_data", function (err, data) {
+		if (err) {
 			console.log("Error reading vk_data");
 		}
 		vk = data.toString().replace(/\n/g, " ").split(" ");
-		console.log("vk_data:");
-		console.log(vk);
 	});
-	fs.readFile("../../build/proof_data", function(err, data) {
-		if(err) {
+	fs.readFile("../../build/proof_data", function (err, data) {
+		if (err) {
 			console.log("Error reading proof_data");
 		}
 		proof = data.toString().replace(/\n/g, " ").split(" ");
-		console.log("proof_data:");
-		console.log(proof);
 	});
 
-	it("should have vk and proof data", function() {
+	it("should have vk and proof data", function () {
 		assert.isAtLeast(vk.length, 24, "vk_data not correct size");
 		assert.isAtLeast(proof.length, 18, "proof_data not correct size");
 	});
 
-	it("should set verifying key", function() {
+	it("should set verifying key", function () {
 		var verifier;
-		
-		return Verifier.deployed().then(function(instance) {
+
+		return Verifier.deployed().then(function (instance) {
 			verifier = instance;
-		}).then(function() {
+		}).then(function () {
 			A = parseG2Point(vk);
 			vk = vk.slice(4);
 			B = parseG1Point(vk);
@@ -55,28 +51,27 @@ contract('Verifier', function(accounts) {
 			vk = vk.slice(4);
 			Z = parseG2Point(vk);
 			vk = vk.slice(4);
-			while(vk != [] && vk[0] != "") {
+			while (vk != [] && vk[0] != "") {
 				IC.push(parseG1Point(vk));
 				vk = vk.slice(2);
 			}
-
 			return verifier.setVerifyingKey(A, B, C,
-											gamma, gamma_beta_1, gamma_beta_2,
-											Z,
-											IC);
-		}).then(function() {
+				gamma, gamma_beta_1, gamma_beta_2,
+				Z,
+				IC);
+		}).then(function () {
 			return verifier.verifyingKeySet.call();
-		}).then(function(verifyingKeySet) {
+		}).then(function (verifyingKeySet) {
 			assert.equal(verifyingKeySet, true, "Verification key not set");
 		});
 	});
 
-	it("should verify correct proof", function() {
+	it("should verify correct proof", function () {
 		var verifier;
 
-		return Verifier.deployed().then(function(instance) {
+		return Verifier.deployed().then(function (instance) {
 			verifier = instance;
-		}).then(function() {
+		}).then(function () {
 			A_g = parseG1Point(proof);
 			proof = proof.slice(2);
 			A_h = parseG1Point(proof);
@@ -97,35 +92,41 @@ contract('Verifier', function(accounts) {
 			return verifier.verifyTx.call(
 				A_g, A_h, B_g, B_h, C_g, C_h,
 				H, K,
-				['0xeffd0b7f1ccba1162ee816f731c62b4859305141990e5c0ace40d33d0b1167d1']
+				[
+					'25028889982724139076220695786004987895',
+					'185959694637158821642035305552923987098'
+				]
 			);
-		}).then(function(result) {
+		}).then(function (result) {
 			assert.equal(result, true, "The correct proof did not verify");
 		});
 	});
 
-	it("shouldn't verify incorrect proof", function() {
+	it("shouldn't verify incorrect proof", function () {
 		var verifier;
 
 		var wrongProof, wrongInput;
 
-		return Verifier.deployed().then(function(instance) {
+		return Verifier.deployed().then(function (instance) {
 			verifier = instance;
 
 			return verifier.verifyTx.call(
-				[0,0], A_h, B_g, B_h, C_g, C_h,
+				[0, 0], A_h, B_g, B_h, C_g, C_h,
 				H, K,
-				['0xeffd0b7f1ccba1162ee816f731c62b4859305141990e5c0ace40d33d0b1167d1']
+				[
+					'25028889982724139076220695786004987895',
+					'185959694637158821642035305552923987098'
+				]
 			);
-		}).then(function(result) {
+		}).then(function (result) {
 			wrongProof = result;
 
 			return verifier.verifyTx.call(A_g, A_h, B_g, B_h, C_g, C_h,
-										  H, K,
-										  [0]);
-		}).then(function(result) {
+				H, K,
+				[0,1]);
+		}).then(function (result) {
 			wrongInput = result;
-		}).then(function() {
+		}).then(function () {
 			assert.equal(wrongProof, false, "The incorrect proof was verified");
 			assert.equal(wrongInput, false, "The incorrect input was verified");
 		});
